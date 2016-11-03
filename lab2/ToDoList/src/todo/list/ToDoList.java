ac8df786b2dddf771ba7861b9d9b1c65ab3a0fa4
katherine11 <katherine.simeonova@gmail.com>
2016-11-03 23:25:24 +0200

    Repairing the solution for the second task

--- lab2/ToDoList/src/todo/list/ToDoList.java
+++ lab2/ToDoList/src/todo/list/ToDoList.java
@@ -4,20 +4,24 @@ import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.Scanner;
 
+import javax.xml.crypto.URIReference;
+
 public class ToDoList {
 
+	private static final int FIRST_VALUE = 1;
+	private static final int EXIT_VALUE = 4;
 	private static final int PERIOD_OF_UPCOMING_TASKS = 3;
 	private static final int NUMBER_OF_TASKS = 7;
 	Task[] tasks = new Task[NUMBER_OF_TASKS];
 
 	public ToDoList() {
 
-		String[] names = { "Clean", "Go to fitness", "To pay the bills"};
+		String[] names = { "Clean", "Go to fitness", "To pay the bills" };
 
 		for (int index = 0; index < this.tasks.length; index++) {
 
 			String randomName = names[(int) (Math.random() * names.length)];
-			TaskStatus randomStatus = TaskStatus.values()[(int) (Math.random()*TaskStatus.values().length)];
+			TaskStatus randomStatus = TaskStatus.values()[(int) (Math.random() * TaskStatus.values().length)];
 			byte randomPriority = (byte) (Math.random() * 5 + 1);
 			LocalDate randomDate = LocalDate.now().plusDays((int) (Math.random() * 10 + 2));
 
@@ -37,57 +41,77 @@ public class ToDoList {
 
 	void printTasksWithStatus(TaskStatus status) {
 
-//		Task[] tasksCopy = new Task[NUMBER_OF_TASKS];
-		
 		int resultLength = 0;
-		for (int index = 0; index < this.tasks.length; index++) {			
+		for (int index = 0; index < this.tasks.length; index++) {
 			if (this.tasks[index].getStatus().equals(status)) {
-//				tasksCopy[taskIndex++] = this.tasks[index];
 				resultLength++;
 			}
 		}
-		
-		Task [] result = returnTaskStatusResult(this.tasks, resultLength);
+
+		Task[] result = returnTaskStatusResult(this.tasks, resultLength, status);
 		Arrays.sort(result);
-		
+
 		printTasks(result);
 
 	}
 
-	private Task[] returnTaskStatusResult(Task[] tasksCopy, int length) {
-		Task [] result = new Task[length];
-		
+	private Task[] returnTaskStatusResult(Task[] tasksCopy, int length, TaskStatus status) {
+		Task[] result = new Task[length];
+
 		int taskIndex = 0;
-		
+
 		for (int index = 0; index < tasksCopy.length; index++) {
-			if(tasksCopy[index].getStatus().equals(TaskStatus.IN_PROCESS)){
+			if (tasksCopy[index].getStatus().equals(status)) {
 				result[taskIndex++] = tasksCopy[index];
 			}
 		}
-		
+
 		return result;
 	}
 
 	void tasksWithDeadline(byte numberOfDays) {
 
-		Task[] tasksCopy = new Task[NUMBER_OF_TASKS];
-
+		int resultLength = 0;
+		
 		for (int index = 0; index < this.tasks.length; index++) {
 
+			//TODO:repair the deadline!!
 			LocalDate currentDeadline = this.tasks[index].getDeadline();
 
 			if (checkForValidTask(numberOfDays, index, currentDeadline)) {
-				tasksCopy[index] = this.tasks[index];
+				resultLength++;
 			}
 
 		}
 
-		printTasks(tasksCopy);
+		Task [] result = returnUpcomingTasksResult(this.tasks, resultLength, numberOfDays);
+		Arrays.sort(result);
+		printTasks(result);
 
 	}
 
+	private Task[] returnUpcomingTasksResult(Task[] tasksCopy, int length, byte numberOfDays) {
+		Task[] result = new Task[length];
+
+		int taskIndex = 0;
+		
+		for (int index = 0; index < tasksCopy.length; index++) {
+			LocalDate currentDeadline = this.tasks[index].getDeadline();
+
+			if (checkForValidTask(numberOfDays, index, currentDeadline)) {
+				result[taskIndex++] = tasksCopy[index];
+			}
+		}
+		
+		
+		return result;
+	}
+
 	private boolean checkForValidTask(byte numberOfDays, int index, LocalDate currentDeadline) {
-		return currentDeadline.isBefore(currentDeadline.plusDays(numberOfDays))
+		
+		LocalDate today = LocalDate.now();
+		
+		return today.plusDays(numberOfDays).compareTo(currentDeadline) < 0
 				&& (this.tasks[index].getStatus().equals(TaskStatus.INITIAL)
 						|| this.tasks[index].getStatus().equals(TaskStatus.IN_PROCESS));
 	}
@@ -113,7 +137,7 @@ public class ToDoList {
 
 			System.out.println("Choose between 1 and 4: ");
 			option = scanner.nextByte();
-			
+
 			switch (option) {
 
 			case 1:
@@ -125,15 +149,14 @@ public class ToDoList {
 			case 3:
 				tasksWithDeadline((byte) PERIOD_OF_UPCOMING_TASKS);
 				break;
-			case 4: 
+			case 4:
 				System.out.println("Exiting...");
 				break;
 			default:
 				System.out.println("There is not such an option!");
 			}
-		
-		} while (option != 4 || option < 1 || option > 4);
-		
+
+		} while ((option < FIRST_VALUE || option > EXIT_VALUE) || option != EXIT_VALUE);
 
 	}
 
