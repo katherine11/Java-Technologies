997c1c6814aa81475144ed040f1433c6da2c34cf
katherine11 <katherine.simeonova@gmail.com>
2016-12-31 10:14:25 +0200

    Update homework 4

--- lab4/src/translate/application/TextArchiver.java
+++ lab4/src/translate/application/TextArchiver.java
@@ -1,14 +1,9 @@
 package translate.application;
 
-import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.util.zip.GZIPOutputStream;
 
 public class TextArchiver {
 
@@ -30,24 +25,6 @@ public class TextArchiver {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-
-		// try(FileOutputStream oStream = new FileOutputStream("translation")){
-		//
-		// oStream.write(text.getBytes());
-		// oStream.flush();
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-
-		// writes the last line only:
-		// try (PrintStream out = new PrintStream(new
-		// FileOutputStream("filename.txt"))) {
-		// out.print(text);
-		// } catch (FileNotFoundException e) {
-		// e.printStackTrace();
-		// }
 
 		try (FileWriter writer = new FileWriter("translation")) {
 			writer.write(text);
